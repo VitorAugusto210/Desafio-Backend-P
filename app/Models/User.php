742b792618daf522df6fcf668dc475e2490bb7ff
Vitor Augusto <vitoraug210@gmail.com>
2025-10-21 08:10:31 -0300

    Finalizando login e autenticação

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,9 +7,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
     use HasFactory, Notifiable, HasRoles;
     /**
      * The attributes that are mass assignable.
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,19 @@
 
 use App\Http\Controllers\Api\AuthController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    // Rota de teste
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    // Proximos cruds e rotas protegidas aqui
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
